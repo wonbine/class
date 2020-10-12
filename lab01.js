efd7413cd4243cfaf2d47034c798581ab8681857
wonbine <71205852+wonbine@users.noreply.github.com>
2020-10-12 21:00:07 +0900

    Add files via upload

---
+++ lab01.js
@@ -0,0 +1,15 @@
+		function f1(x){
+			x.style.background='indigo';
+			x.style.width = "300px";
+			x.style.height = "300px";
+		}
+		function f2(x){
+			x.style.background='gray';
+			x.style.width='500px'
+			x.style.height='500px'
+			}
+			function over(x1){
+				x1.src="../week06/myimage.jpg";}
+			function out(x1){
+				x1.src="../week06/myimage2.jpg";
+			}
